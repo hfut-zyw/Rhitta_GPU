dff20394313689e20f410f852ffe4bf550d8c8e5
hfut-zyw <lucario@qq.com>
2023-05-03 19:34:52 +0800

    数据集

---
+++ data/test_DataLoader.py
@@ -0,0 +1,22 @@
+import rhitta.nn as nn
+import cupy as cp
+
+"""
+Mnist数据集载入
+"""
+# mnist=nn.MnistLoader()
+# data=mnist.load(r"D:\Rhitta_GPU\data\dataset")
+# x,labels=data
+# print(x.shape,labels.shape,type(x))
+# print(x[0].shape)
+# print(labels[0])
+
+"""
+IMDB数据集载入
+"""
+# imdb=nn.IMDBLoader()
+# data=imdb.load(r"D:\Rhitta_GPU\data\dataset")
+# x,labels=data
+# print(len(x),len(labels),type(x))
+# print(x[0])
+# print(labels[0])
